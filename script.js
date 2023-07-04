ddc7c8662c125fe611f694128818b8d98446f53b
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-07-04 12:03:20 -0400

    Function renderPuzzel added

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ const puzzleContainer = document.querySelector("#puzzle-container")
                 }
                 return col
             }
+            function generatePuzzle() {
+                for (let i = 1; i <= size * size; i++) {
+                    puzzle.push({
+                        value: i,
+                        position: i,
+                        x: (getCol(i) - 1) * 200,
+                        y: (getRow(i) - 1) * 200,
+                        disabled: false,
+                    })
+                }
+            }
+
+            function renderPuzzle() {
+                puzzleContainer.innerHTML = ""
+                for (let puzzleItem of puzzle) {
+                    if (puzzleItem.disabled) continue
+                    puzzleContainer.innerHTML += `
+                        <div class="puzzle-item" style="left: ${puzzleItem.x}px; top: ${puzzleItem.y}px;">
+                            ${puzzleItem.value}
+                        </div>
+                    `
+                }
+            }
+
+            
