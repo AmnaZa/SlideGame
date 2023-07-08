401148e82930244a7d2a789b0619a639df666a3d
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-07-08 12:34:02 -0400

    function to change directions added

--- script.js
+++ script.js
@@ -113,6 +113,73 @@ const puzzleContainer = document.querySelector("#puzzle-container")
                     swapPositions(emptyPuzzle, abovePuzzle, false)
                 }
             }
+            function swapPositions(firstPuzzle, secondPuzzle, isX = false) {
+                // position swapping
+                let temp = firstPuzzle.position
+                firstPuzzle.position = secondPuzzle.position
+                secondPuzzle.position = temp
 
+                // x position swapping
+
+                if (isX) {
+                    temp = firstPuzzle.x
+                    firstPuzzle.x = secondPuzzle.x
+                    secondPuzzle.x = temp
+                } else {
+                    // must be y
+                    temp = firstPuzzle.y
+                    firstPuzzle.y = secondPuzzle.y
+                    secondPuzzle.y = temp
+                }
+            }
+
+            function getRightPuzzle() {
+                /* get the puzzle just right to the empty puzzle */
+                const emptyPuzzle = getEmptyPuzzle()
+                const isRightEdge = getCol(emptyPuzzle.position) === size
+                if (isRightEdge) {
+                    return null
+                }
+                const puzzle = getPuzzleByPos(emptyPuzzle.position + 1)
+                return puzzle
+            }
+            function getLeftPuzzle() {
+                /* get the puzzle just left to the empty puzzle */
+                const emptyPuzzle = getEmptyPuzzle()
+                const isLeftEdge = getCol(emptyPuzzle.position) === 1
+                if (isLeftEdge) {
+                    return null
+                }
+                const puzzle = getPuzzleByPos(emptyPuzzle.position - 1)
+                return puzzle
+            }
+            function getAbovePuzzle() {
+                /* get the puzzle just above to the empty puzzle */
+                const emptyPuzzle = getEmptyPuzzle()
+                const isTopEdge = getRow(emptyPuzzle.position) === 1
+                if (isTopEdge) {
+                    return null
+                }
+                const puzzle = getPuzzleByPos(emptyPuzzle.position - size)
+                return puzzle
+            }
+            function getBelowPuzzle() {
+                /* get the puzzle just below to the empty puzzle */
+                const emptyPuzzle = getEmptyPuzzle()
+                const isBottomEdge = getRow(emptyPuzzle.position) === size
+                if (isBottomEdge) {
+                    return null
+                }
+                const puzzle = getPuzzleByPos(emptyPuzzle.position + size)
+                return puzzle
+            }
+
+            function getEmptyPuzzle() {
+                return puzzle.find((item) => item.disabled)
+            }
+
+            function getPuzzleByPos(pos) {
+                return puzzle.find((item) => item.position === pos)
+            }
 
             
